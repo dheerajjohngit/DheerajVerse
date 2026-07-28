b47c5f430266e550c45b9c27edfe6ce268db0be0
dheerajjohngit <dheerajjohncr777@gmail.com>
2026-07-28 11:52:07 +0530

    Initial React Vite setup

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,51 @@
+import "./Hero.css";
+
+function Hero(){
+
+return(
+
+<section className="hero">
+
+<div className="left">
+
+<h1>
+
+DHEERAJ
+
+<br/>
+
+JOHN
+
+</h1>
+
+<p>
+
+Python Full Stack Developer
+
+AI Engineer
+
+Data Scientist
+
+</p>
+
+<button>
+
+Explore My Work
+
+</button>
+
+</div>
+
+<div className="right">
+
+IMAGE HERE
+
+</div>
+
+</section>
+
+)
+
+}
+
+export default Hero;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+
+      <div className="logo">
+
+        DheerajVerse
+
+      </div>
+
+      <ul>
+
+        <li>Home</li>
+
+        <li>About</li>
+
+        <li>Experience</li>
+
+        <li>Projects</li>
+
+        <li>Skills</li>
+
+        <li>Contact</li>
+
+      </ul>
+
+      <button>
+
+        Hire Me
+
+      </button>
+
+    </nav>
+  );
+}
+
+export default Navbar;
